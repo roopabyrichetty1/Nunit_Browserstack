79c027ab0438dd84195a42c07f0e24995f7f6785
Roopa Byrichetty <roopab77@yahoo.com>
2023-10-12 14:24:36 -0400

    After debug

--- NUnit-BrowserStack/SampleTest.cs
+++ NUnit-BrowserStack/SampleTest.cs
@@ -12,15 +12,9 @@ namespace BrowserStack
         [Test]
         public void SearchBstackDemo()
         {
-            driver.Navigate().GoToUrl("https://bstackdemo.com/");
-            Assert.AreEqual("StackDemo", driver.Title);
-            string productOnPageText = driver.FindElement(By.XPath("//*[@id=\"1\"]/p")).Text;
-
-            driver.FindElement(By.XPath("//*[@id=\"1\"]/div[4]")).Click();
-            bool cartOpened = driver.FindElement(By.XPath("//*[@class=\"float-cart__content\"]")).Displayed;
-            Assert.AreEqual(cartOpened, true);
-            string productOnCartText = driver.FindElement(By.XPath("//*[@id=\"__next\"]/div/div/div[2]/div[2]/div[2]/div/div[3]/p[1]")).Text;
-            Assert.AreEqual(productOnCartText, productOnPageText);
+            driver.Navigate().GoToUrl("https://www.bentley.com/");
+            Assert.AreEqual("Home | Bentley | Infrastructure Engineering Software", driver.Title);          
+            
         }
     }
 }
